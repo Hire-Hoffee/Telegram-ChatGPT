f8fa1f8467376a263d34297483814a1d5a485313
Rostislav Alekseev <mr.rostik.01@gmail.com>
2023-03-21 01:47:56 +0200

    reduce context length

--- textMessages.js
+++ textMessages.js
@@ -1,7 +1,7 @@
 const fillerText = {
   greetings: `This bot has ChatGPT artificial intelligence features. You can ask him various questions and he may even answer them correctly.
 
-  This bot can remember the context of the messages, but the context is limited to 30 messages. After reaching this limit, a message will be displayed and the memory of previous requests will be cleared.
+  This bot can remember the context of the messages, but the context is limited to 10 messages. After reaching this limit, a message will be displayed and the memory of previous requests will be cleared.
   You can manually clear context by sending '/resetcontext' command.
   `,
 };
