b1da5bd9db4a0648c9bdfef4325146be6624faf6
Rostislav Alekseev <mr.rostik.01@gmail.com>
2023-03-21 21:38:18 +0200

    messages count

--- utils.js
+++ utils.js
@@ -10,19 +10,25 @@ async function usersTracking(filePath, msg) {
   const fileData = JSON.parse(await fs.readFile(filePath));
   const userData = {
     username: msg.from.username,
-    message: msg.text,
+    numOfMessages: 1,
+    message: msg.text.length >= 75 ? `${msg.text.slice(0, 75)}...` : msg.text,
     message_date: new Date(msg.date * 1000).toLocaleString(),
     language_code: msg.from.language_code,
     is_bot: msg.from.is_bot,
   };
 
   if (fileData.some((user) => user.username === msg.from.username)) {
+    fileData.forEach((user) => {
+      if (user.username === msg.from.username) {
+        user.numOfMessages += 1;
+      }
+    });
+    await fs.writeFile(filePath, JSON.stringify(fileData));
     return;
   }
 
   fileData.push(userData);
   await fs.writeFile(filePath, JSON.stringify(fileData));
-
   return;
 }
 
